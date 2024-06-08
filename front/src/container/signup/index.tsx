6f41e90a74c48d8acd0d9af6269bb83b1e7cb47f
Albina <yang679.lit@gmail.com>
2024-06-08 16:50:54 +0300

    rollback

--- front/src/container/signup/index.tsx
+++ front/src/container/signup/index.tsx
@@ -1,8 +1,6 @@
 import { useContext } from "react";
 
-import "../../global.scss"
 import "./index.scss";
-
 import { useNavigate } from "react-router-dom";
 
 const SignUpPage: React.FC = () => {
@@ -15,37 +13,7 @@ const SignUpPage: React.FC = () => {
         <div>
           <img src="img/logo.png" alt="logo" className="logo" />
         </div>
-        <div><h1 className="title text">Захоплюючі розмови на теми, що вас цікавлять!</h1>
-        <p className="text">Наш чат створений для того, щоб об'єднати людей з різними  інтересами і допомогти відкривати нові хобі!</p></div>
-        <div>
-          <img src="img/mcbook.png" alt="mcbook" className="aside_img" />
-        </div>
       </aside>
-      <section className="page__section form__section">
-        <form className="form__container">
-        <h2 className="title">Зареєструватися</h2>
-        
-        <div className="field">
-          <label htmlFor="firstname">Ім'я</label>
-        <input name="firstname" id="firstname" type="text" placeholder="Введіть ваше ім’я" />
-        </div>
-        <div className="field">
-          <label htmlFor="email">Електронна адреса</label>
-        <input name="email" id="email" type="email" placeholder="Введіть вашу електронну адресу" />
-        </div>
-        <div className="field">
-          <label htmlFor="password">Пароль</label>
-        <input name="password" id="password" type="password" placeholder="Створіть пароль" />
-        </div>
-        <button className="button button--dark" type="submit">Продовжити</button>
-        </form>
-      <div className="signin">
-        <p className="text--small">
-        У вас є обліковий запис?
-        <a className="link" href="/">{" "}Увійти</a>
-        </p>
-      </div>
-      </section>
       <form className="page__section">Form</form>
     </div>
   );
