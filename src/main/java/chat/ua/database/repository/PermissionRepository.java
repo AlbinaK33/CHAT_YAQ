d7b33d26710ca931477319fa7d7512e763f96c3f
Albina <yang679.lit@gmail.com>
2024-06-08 16:59:10 +0300

    Update project

---
+++ src/main/java/chat/ua/database/repository/PermissionRepository.java
@@ -0,0 +1,9 @@
+package chat.ua.database.repository;
+
+import chat.ua.database.entity.Permission;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PermissionRepository extends JpaRepository<Permission, Long> {
+}
