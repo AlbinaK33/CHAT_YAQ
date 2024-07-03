6469f2f795b783bd2e6f4777551fa4f712231ec0
Kateryna Lysenko <katrikenko@gmail.com>
2024-07-03 15:27:14 +0200

    fixed some styles

--- front/src/component/field-password/index.tsx
+++ front/src/component/field-password/index.tsx
@@ -6,6 +6,7 @@ interface PasswordProps {
     value: string;
     onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
     error: string;
+    requirements: string;
     showPassword: boolean;
     placeholder: string;
     label: string;
@@ -17,6 +18,7 @@ const FieldPassword: React.FC<PasswordProps> = ({
     value, 
     onChange,
     error,
+    requirements,
     showPassword,
     onTogglePassword,
     placeholder,
@@ -43,6 +45,14 @@ const FieldPassword: React.FC<PasswordProps> = ({
             <p><span className="form__error" id="passwordError">  {error}</span></p>
             </div>
         )}
+        </div>
+        <div className="password-requirements">
+            {requirements.map((req, index) => {
+                <div key={index} className="requirement">
+                    <span className={`icon ${req.isMet} ? "requirement-success" : "requirement-error"`}></span>
+                    <span>{req.text}</span>
+                </div>
+            })}
         </div>
         </div>
     )
