f28775dd4ea176506d141190da3c7241d0b52575
Kateryna Lysenko <katrikenko@gmail.com>
2024-08-09 19:40:11 +0200

    added timezones

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -6,7 +6,6 @@ import SignUpPage from "./container/signup";
 import SignInPage from "./container/signin";
 import SignNamePage from "./container/signName";
 
-import { createContext } from "vm";
 import OnboardingPage from "./page/onboarding"
 import RecoveryPage from "./container/recoveryPage";
 import RecoveryPasswordPage from "./container/recoveryPassword";
@@ -17,22 +16,12 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<OnboardingPage />} />
+      <Route path="/" element={<OnboardingPage />} />
         <Route path="/signup" element={<SignUpPage />} />
-      </Routes>
-      <Routes>
         <Route path="/signin" element={<SignInPage />} />
-      </Routes>
-      <Routes>
         <Route path="/signname" element={<SignNamePage />} />
-      </Routes>
-      <Routes>
         <Route path="/timezone" element={<TimezonePage />} />
-      </Routes>
-      <Routes>
         <Route path="/recovery" element={<RecoveryPage />} />
-      </Routes>
-      <Routes>
         <Route path="/recoveryPassword" element={<RecoveryPasswordPage />} />
       </Routes>
     </BrowserRouter>
